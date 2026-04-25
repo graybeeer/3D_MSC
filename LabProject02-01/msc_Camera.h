c6ee4a70c0874e19286e3dc4d280dc24826cd960
회색곰발 <sunim555@naver.com>
2026-04-25 21:36:11 +0900

    26-04-25

--- LabProject02-01/msc_Camera.h
+++ LabProject02-01/msc_Camera.h
@@ -4,15 +4,23 @@
 class msc_Viewport;
 class msc_Camera : public msc_Component
 {
-	public:
+public:
 	msc_Camera(msc_GameObject* pParentObject);
 	virtual ~msc_Camera();
 	void Start() override;
 	void Update() override;
 	void onDestroy() override;
 
+	msc_Viewport m_Viewport;
+
 private:
-	msc_Viewport* m_Viewport;
+	float						m_fFOVAngle = 90.0f;
+	float						m_fProjectRectDistance = 1.0f;
+
+	//카메라 오브젝트의 위치와 회전만 카메라의 뷰 행렬에 영향. scale은 무시
+	BoundingFrustum				m_xmFrustumView = BoundingFrustum();
+	BoundingFrustum				m_xmFrustumWorld = BoundingFrustum();
+	XMFLOAT4X4					m_xmf4x4InverseView = Matrix4x4::Identity();
 
 };
 
--- LabProject02-01/msc_GameManager.h
+++ LabProject02-01/msc_GameManager.h
@@ -7,13 +7,6 @@ class msc_GameManager : public msc_Component
 {
 
 public:
-	static msc_GameManager* GetInstance(msc_GameObject* pParentObject) //싱글톤 
-	{
-		static msc_GameManager instance(pParentObject);
-		return &instance;
-	}
-	msc_GameManager(const msc_GameManager&) = delete; // 복사 생성자 삭제
-	msc_GameManager& operator=(const msc_GameManager&) = delete; // 대입 연산자 삭제
 
 public:
 	msc_GameManager(msc_GameObject* pParentObject);
